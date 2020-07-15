2b398203a1cbf7e4ecde8baf870d10ffb67e2d95
Petras Slekys <Petras.Slekys@ibm.com>
2020-07-15 21:56:35 +0300

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="header">
+    <img
+      src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+      alt="Google"
+    />
+
+    <nav>
+      <a href="#" v-for="link in links" :key="link">{{ link }}</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      links: ["Home", "About", "Contacts"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+img {
+  height: 40px;
+  padding: 5px 0;
+}
+
+.header {
+  background: #eee;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+a {
+  margin-right: 10px;
+}
+
+a:last-child {
+  margin-right: 0;
+}
+</style>
